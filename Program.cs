b3e76a791663533ca1f720e388effbdc08134725
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-23 18:44:22 +0530

    Added : Main Method to call Another Methods

--- Program.cs
+++ Program.cs
@@ -7,8 +7,12 @@ namespace FindMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Find max Number");
+            
             int output = MaximumNumberCheck.MaximumIntegerNumber(23, 43, 65);
             Console.WriteLine(output);
+            
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
         }
     }
 }
