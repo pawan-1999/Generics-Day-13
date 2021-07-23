b3100e633b4e4c2031d804bd7e4ca8329794a2fd
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-23 19:18:19 +0530

    Added : Main Method to  Call Another Method

--- Program.cs
+++ Program.cs
@@ -7,15 +7,16 @@ namespace FindMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Finding max Number :");
-            
-            string maximum1 = MaximumNumberCheck.FindMaximum<int>(29, 76, 94);
-            string maximum2 = MaximumNumberCheck.FindMaximum<float>(231.2f,112.45f,32.56f);
-            string maximum3 = MaximumNumberCheck.FindMaximum<string>("202", "323", "441");
-
-            Console.WriteLine(maximum1);
-            Console.WriteLine(maximum2);
-            Console.WriteLine(maximum3);
-            
+           
+            int[] intArray = { 242, 112, 912, 220, 178 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 21.2, 34.4, 14.32, 25.5, 16.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "101", "422", "233", "552", "109" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
             
         }
     }
